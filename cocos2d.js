c6d0a84f0c7b96de9170179b50f1d3a4cfb823c4
Erik Wittkorn <erik.wittkorn@gmail.com>
2013-02-26 13:28:28 +0100

    Hello World

---
+++ cocos2d.js
@@ -0,0 +1,36 @@
+(function() {
+    var d = document;
+    var c = {
+        COCOS2D_DEBUG:2, // 0 - off, 1 - basic, 2 - full
+        box2d:false,
+        chipmunk:false,
+        showFPS:true,
+        frameRate:60,
+        loadExtension:false,
+        tag:'gameCanvas',
+        engineDir:'lib/cocos2d/',
+        appFiles:['script/MyFirstApp.js']
+    };
+    window.addEventListener('DOMContentLoaded', function (){
+        //first load engine file if specified
+        var s = d.createElement('script');
+        /*********Delete this section if you have packed all files into one*******/
+        if (c.SingleEngineFile && !c.engineDir) {
+            s.src = c.SingleEngineFile;
+        }
+        else if (c.engineDir && !c.SingleEngineFile) {
+            s.src = c.engineDir + 'platform/jsloader.js';
+        }
+        else {
+            alert('You must specify either the single engine file OR the engine directory in "cocos2d.js"');
+        }
+        /*********Delete this section if you have packed all files into one*******/
+
+            //s.src = 'Packed_Release_File.js'; //IMPORTANT: Un-comment this line if you have packed all files into one
+
+        document.ccConfig = c;
+        s.id = 'cocos2d-html5';
+        d.body.appendChild(s);
+        //else if single file specified, load singlefile
+    });
+})();
